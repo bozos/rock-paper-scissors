124c1cff218f13600439ae5354f48a6fd81e93e6
Jai Luthra <jailuthra6sep@gmail.com>
2013-01-27 15:33:11 +0530

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function cpuPick()
 {
     var possible = "rps";
-    var text = possible.charAt(Math.floor(Math.random());
+    var text = possible.charAt(Math.floor(Math.random() * 1));
     return text;
 }
